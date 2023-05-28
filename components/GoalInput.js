a78828de39827b50b1049ca8a75784b4f71224b3
sonukanojiya <rituknj@gmail.com>
2023-05-28 17:11:24 +0530

    Initial commit

---
+++ components/GoalInput.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { Text, View, Button, StyleSheet, TextInput, Modal, Image } from "react-native";
+
+function GoalInput(props) {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+  function addGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText("");
+  }
+ 
+  return (
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
+      <Image style={styles.image} source={require('../assets/Image/goal.png')} />
+        <TextInput
+          style={styles.textInput}
+          placeholder="$1000"
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Invest" onPress={addGoalHandler} color="#b180f0"/>
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" onPress={props.onCancel} color='#f31282'/>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+export default GoalInput;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding:16,
+    backgroundColor: '#311b6b'
+  },
+  image:{
+    width: 100,
+    height: 100,
+    margin: 20
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#e4d0ff",
+    width: "100%",
+    backgroundColor: "#e4d0ff",
+    color: '#120438',
+    borderRadius:6,
+    padding: 16,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 16
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
+  },
+});
